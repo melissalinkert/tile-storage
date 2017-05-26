ad7cdb6237b4a7adce82c23132d40b2ef6371c96
Melissa Linkert <melissa@glencoesoftware.com>
2017-05-25 21:23:24 -0500

    Update tile reading example to allow reading all tiles

--- src/main/java/ome/tiles/cli/ReadTile.java
+++ src/main/java/ome/tiles/cli/ReadTile.java
@@ -27,6 +27,7 @@ import net.sourceforge.argparse4j.inf.ArgumentParserException;
 import ome.tiles.metadata.IMetadataStorage;
 import ome.tiles.metadata.JSONMetadataStorage;
 import ome.tiles.metadata.Metadata;
+import ome.tiles.metadata.Projection;
 import ome.tiles.metadata.TileKey;
 import ome.tiles.io.DiskTileStorage;
 import ome.tiles.io.FilesystemStorageService;
@@ -39,16 +40,16 @@ public class ReadTile {
   private String inputDirectory;
 
   @Arg
-  private int x;
+  private Integer x;
 
   @Arg
-  private int y;
+  private Integer y;
 
   @Arg
-  private int width;
+  private Integer width;
 
   @Arg
-  private int height;
+  private Integer height;
 
   public void process() {
     // for now, assume local filesystem backend
@@ -64,33 +65,53 @@ public class ReadTile {
 
       Metadata metadata = metadataStorage.load();
       long t1 = System.currentTimeMillis();
-      TileKey key = new TileKey(metadata, 0);
-      key.setResolution(0);
-      key.setPlane(0);
-      key.setHorizontalCoordinate(x);
-      key.setVerticalCoordinate(y);
-      key.setTileWidth(width);
-      key.setTileHeight(height);
-      long t2 = System.currentTimeMillis();
-
-      byte[] tile = tileStorage.readTile(key);
-      long t3 = System.currentTimeMillis();
       System.out.println("initial load time = " + (t1 - t0) + " ms");
-      System.out.println("key setup time = " + (t2 - t1) + " ms");
-      System.out.println("tile read time = " + (t3 - t2) + " ms");
+
+      if (x == null && y == null && width == null && height == null) {
+        // iterate over all tiles
+        int w = metadata.getTileWidth();
+        int h = metadata.getTileHeight();
+        Projection p = metadata.getProjection(0);
+        for (int yy=0; yy<p.getVerticalTileCount(); yy++) {
+          for (int xx=0; xx<p.getHorizontalTileCount(); xx++) {
+            readTile(tileStorage, metadata, xx * w, yy * h, w, h);
+          }
+        }
+      }
+      else {
+        readTile(tileStorage, metadata, x, y, width, height);
+      }
     }
     catch (IOException e) {
       e.printStackTrace();
     }
   }
 
+  private void readTile(ITileStorage tileStorage, Metadata metadata, int x, int y, int w, int h)
+    throws IOException
+  {
+    long t0 = System.currentTimeMillis();
+    TileKey key = new TileKey(metadata, 0);
+    key.setResolution(0);
+    key.setPlane(0);
+    key.setHorizontalCoordinate(x);
+    key.setVerticalCoordinate(y);
+    key.setTileWidth(w);
+    key.setTileHeight(h);
+    long t1 = System.currentTimeMillis();
+    byte[] tile = tileStorage.readTile(key);
+    long t2 = System.currentTimeMillis();
+    System.out.println("key setup time = " + (t1 - t0) + " ms");
+    System.out.println("tile read time = " + (t2 - t1) + " ms");
+  }
+
   public static void main(String[] args) {
     ArgumentParser parser = ArgumentParsers.newArgumentParser("read-tile");
     parser.addArgument("inputDirectory");
-    parser.addArgument("x").type(Integer.class);
-    parser.addArgument("y").type(Integer.class);
-    parser.addArgument("width").type(Integer.class);
-    parser.addArgument("height").type(Integer.class);
+    parser.addArgument("-x").type(Integer.class).setDefault((Integer) null);
+    parser.addArgument("-y").type(Integer.class).setDefault((Integer) null);
+    parser.addArgument("-width").type(Integer.class).setDefault((Integer) null);
+    parser.addArgument("-height").type(Integer.class).setDefault((Integer) null);
 
     ReadTile reader = new ReadTile();
     try {
